bfac80e76193b878847191f32b4abe06c45540a5
Eduardo Alcantara de Oliveira <eoalcantara@gmail.com>
2022-06-21 20:03:21 -0300

    Update profile.component.ts

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -13,14 +13,9 @@ export class ProfileComponent implements OnInit {
   get profile() {
     return {
       name: "Eduardo Alcantara de Oliveira",
-      title: "Computer Science / Cientista da Computação",
+      title: "Analista desenvolvedor .Net C#",
       description: `Profissional apaixonado por tecnologia e movido pelo propósito de impactar a vida das pessoas, criando ferramentas para que elas possam fazer melhor as suas atividades.
-      <br/><br/>
-      Em minha experiência profissional descobri que mudanças acontecem e que tecnologias evoluem, com isso me tornei um profissional resiliente e flexível. Com essas mudanças é necessário o aprendizado e, para isso, conto com o autodidatismo e a organização. Passar por mudanças sempre é difícil, porém sozinho se torna ainda mais, por isso sempre prezo pela colaboração e o trabalho em equipe, afinal, "juntos somos mais fortes".
-      <br/><br/>
-      Me formei em Ciência da Computação pela Universidade Tecnológica Federal do Paraná (UTFPR - 2015) e me especializei em Inteligência Artificial e Aprendizado de Máquina (Machine Learning) pela FIAP (2020), além de estar sempre me atualizando com cursos, palestras e artigos.
-      <br/><br/>
-      Que tal aquela partidinha de FIFA? 🎮 Ou um joguinho de truco? 🃏 Vamos conversar! 💬`,
+      <br/><br/>   
     };
   }
 
